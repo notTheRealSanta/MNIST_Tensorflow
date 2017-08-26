0260030ba954f837b884dbdf368ca5d9818d7aa3
NotTheRealSanta <ucsantosh@gmail.com>
2017-08-26 07:01:31 +0530

    updated

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # MNIST_Tensorflow
 AI to read numbers from the MNIST data set.
+
+
+## Comparing different types and ways of neural network
+
+|  Accuracy  | Type of NN | Layer 1 | Layer 2 | Layer 3 | Epochs | 
+|:----------:|:----------:|:-------:|:-------:|:-------:|:------:|
+|0.9452      |FF          |784      |784*2    |-        |3       |
+|0.9586      |FF          |784      |784*2    |-        |10        |
+|            |            |         |         |         |        |
+|            |            |         |         |         |        |
+|            |            |         |         |         |        |
+
--- mnist_tf_main.py
+++ mnist_tf_main.py
@@ -13,20 +13,25 @@ nodes_hl2_len = (784*2)
 classes_len = 10
 batch_size = 100
 
-x = tf.placeholder(tf.float32,[None,784])
-y = tf.placeholder(tf.float32)
+x = tf.placeholder(tf.float32,[None,784], name = "x")
+y = tf.placeholder(tf.float32, name="y")
 
 #defining the model of the network
 def neural_network_model (data) :
 
-    hidden_layer_1_weights = tf.Variable(tf.random_normal([784,nodes_hl1_len]))
-    hidden_layer_1_biases  = tf.Variable(tf.random_normal([nodes_hl1_len]))
+    with tf.name_scope ("Layer_1") :
+        hidden_layer_1_weights = tf.Variable(tf.random_normal([784,nodes_hl1_len]), name = "W")
+        hidden_layer_1_biases  = tf.Variable(tf.random_normal([nodes_hl1_len]), name = "B")
 
-    hidden_layer_2_weights = tf.Variable(tf.random_normal([nodes_hl1_len, nodes_hl2_len]))
-    hidden_layer_2_biases  = tf.Variable(tf.random_normal([nodes_hl2_len]))
+    with tf.name_scope ("Layer_2") :
+        hidden_layer_2_weights = tf.Variable(tf.random_normal([nodes_hl1_len, nodes_hl2_len]), name = "W")
+        hidden_layer_2_biases  = tf.Variable(tf.random_normal([nodes_hl2_len]), name = "B")
 
-    output_layer_weights = tf.Variable(tf.random_normal([nodes_hl2_len,classes_len]))
-    output_layer_biases = tf.Variable(tf.random_normal([classes_len]))
+    with tf.name_scope ("Output_Layer") :
+        output_layer_weights = tf.Variable(tf.random_normal([nodes_hl2_len,classes_len]), name = "W")
+        output_layer_biases = tf.Variable(tf.random_normal([classes_len]), name = "B")
+        tf.summary.histogram("weights",output_layer_weights)
+        tf.summary.histogram("biases", output_layer_biases )
 
     l1 = tf.add(tf.matmul(data, hidden_layer_1_weights), hidden_layer_1_biases)
     l1 = tf.nn.relu(l1)
@@ -43,11 +48,13 @@ def train_neural_network (x):
 
     prediction = neural_network_model(x)
 
-    cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y) )
+    with tf.name_scope ("Cost"):
+        cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y) )
+    tf.summary.scalar('cost', cost)
 
     optimizer = tf.train.AdamOptimizer().minimize(cost)
 
-    epoch_len = 3
+    epoch_len = 50
 
     with tf.Session() as sess :
 
@@ -65,11 +72,15 @@ def train_neural_network (x):
             print( 'Epoch',epoch,'/',epoch_len )
 
         #testing the data
-        correct = tf.equal ( tf.argmax(prediction,1), tf.argmax(y,1) )
-        accuracy = tf.reduce_mean ( tf.cast(correct,'float') )
+        with tf.name_scope ("accuracy") :
+            correct = tf.equal ( tf.argmax(prediction,1), tf.argmax(y,1) )
+            accuracy = tf.reduce_mean ( tf.cast(correct,'float') )
+
+        tf.summary.scalar('accuracy', accuracy)
         print ('Accuracy :', accuracy.eval({x:mnist.test.images, y: mnist.test.labels}))
 
-        writer =  tf.summary.FileWriter("/tmp/mnist_demo/1")
+        merged_summary = tf.summary.merge_all()
+        writer =  tf.summary.FileWriter("/tmp/mnist_demo/4")
         writer.add_graph(sess.graph)
 
 
